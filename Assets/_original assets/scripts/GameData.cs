0a05c81904d8ac0e7bf77a4f5da964b4a28b0f7b
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-11-25 13:16:57 -0500

    added SceneLoader code

---
+++ Assets/_original assets/scripts/GameData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SUCC;
+using CommandTerminalPlus;
+
+public static class GameData
+{
+    private static DataFile file = new DataFile("GameData", autoSave: true);
+
+    public static void Set<T>(string key, T value) => file.Set(key, value);
+
+    public static T Get<T>(string key, T defaultValue) => file.Get(key, defaultValue);
+	
+
+    [RegisterCommand(Name = "GameData.Print", Help = "prints the contents of GameData.SUCC", MaxArgCount = 0)]
+    private static void PrintGameData(CommandArg[] args)
+    {
+        Terminal.Log("Contents of GameData.SUCC:");
+        Terminal.Log("");
+
+        foreach(var line in file.GetRawLines())
+            Terminal.Log(line);
+
+        Terminal.Log("");
+    }
+}
