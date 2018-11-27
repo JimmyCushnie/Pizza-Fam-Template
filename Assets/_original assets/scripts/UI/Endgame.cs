2e8924b69a4796e48a6b3270d27c89d53c67fae1
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-11-26 21:33:38 -0500

    added content to Endgame

---
+++ Assets/_original assets/scripts/UI/Endgame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PizzaFam.UI
+{
+    public class Endgame : MonoBehaviour
+    {
+        public void GoToMainMenu() => SceneLoader.LoadMainMenu();
+    }
+}
