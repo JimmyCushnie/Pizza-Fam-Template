6fdbc445141f323abc20dfd2160d6c8eee3c492f
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-11-27 14:40:59 -0500

    improved sound system

--- Assets/_original assets/scripts/Sounds.cs
+++ Assets/_original assets/scripts/Sounds.cs
@@ -19,13 +19,31 @@ namespace PizzaFam
             Instance.Play(NameToSound[soundName]);
         }
 
-        private void Play(Sound sound)
+        public static void PlayVoice(string voiceName)
+        {
+            if (!NameToVoice.ContainsKey(voiceName))
+                throw new Exception($"tried to play uninitialized voice line {voiceName}");
+
+            Instance.Play(NameToVoice[voiceName]);
+        }
+
+        private void Play(SoundEffect sound)
         {
             SoundsSource.pitch = Random.Range(1 - sound.MaxPitchVariation, 1 + sound.MaxPitchVariation);
-            var clip = sound.RandomClips.RandomItem();
-            SoundsSource.PlayOneShot(clip.audio, sound.VolumeScale);
+            SoundsSource.PlayOneShot(sound.clip, sound.VolumeScale);
+        }
 
-            if (!clip.subtitle.IsNullOrEmpty())
+        private void Play(VoiceLine line)
+        {
+            if (Random.Range(0f, 1f) > line.ChanceOfPlaying) return;
+
+            SoundsSource.pitch = 1;
+            var clip = line.RandomClips.RandomItem();
+            SoundsSource.PlayOneShot(clip.audio, line.VolumeScale);
+
+            if (clip.subtitle.IsNullOrEmpty())
+                Debug.LogWarning($"the voice line {line.name} doesn't have a subtitle!");
+            else
                 Subtitles.Say(clip.subtitle, clip.audio.length);
         }
 
@@ -44,31 +62,97 @@ namespace PizzaFam
 
                 NameToSound.Add(sound.name, sound);
             }
+
+            foreach(var voice in VoiceList)
+            {
+                if (NameToVoice.ContainsKey(voice.name))
+                    throw new Exception("error initializing voice lines: duplicate name in list");
+                if (voice.name.IsNullOrEmpty())
+                    throw new Exception("error initializing voice lines: all lines must have a name");
+            }
         }
 
-        private static Dictionary<string, Sound> NameToSound = new Dictionary<string, Sound>();
+        private static Dictionary<string, SoundEffect> NameToSound = new Dictionary<string, SoundEffect>();
+        private static Dictionary<string, VoiceLine> NameToVoice = new Dictionary<string, VoiceLine>();
 
 
         public AudioSource SoundsSource;
 
-        [SerializeField]
-        private Sound[] SoundList;
+        [SerializeField] private List<SoundEffect> SoundList;
+        [SerializeField] private List<VoiceLine> VoiceList;
 
         [Serializable]
-        private class Sound
+        private class SoundEffect
         {
+            public string name;
+            public AudioClip clip;
+            public float MaxPitchVariation;
+            public float VolumeScale;
+
+            public void SetDefaults()
+            {
+                name = "HEY YOU ADD A NAME HERE";
+                MaxPitchVariation = 0;
+                VolumeScale = 1;
+            }
+        }
+
+        [Serializable]
+        private class VoiceLine
+        {
+            public string name = "HEY YOU ADD A NAME HERE";
+            public ClipWithSubtitle[] RandomClips = new ClipWithSubtitle[1];
+            public float VolumeScale = 1;
+            public float ChanceOfPlaying = 1;
+
+            public void SetDefaults()
+            {
+                name = "HEY YOU ADD A NAME HERE";
+                VolumeScale = 1;
+                ChanceOfPlaying = 1;
+            }
+
             [Serializable]
             public class ClipWithSubtitle
             {
                 public AudioClip audio;
                 public string subtitle;
             }
+        }
 
-            public string name = "HEY YOU ADD A SOUND NAME HERE";
-            public ClipWithSubtitle[] RandomClips = new ClipWithSubtitle[1];
 
-            public float MaxPitchVariation = 0;
-            public float VolumeScale = 1;
+        // jesus fucking christ this is WAY more complicated than it needs to be. Get your shit together, unity
+        [HideInInspector] public List<bool> SoundIndexesDefaulted = new List<bool>();
+        [HideInInspector] public List<bool> VoiceIndexesDefaulted = new List<bool>();
+        void OnValidate()
+        {
+            while (SoundIndexesDefaulted.Count > SoundList.Count)
+                SoundIndexesDefaulted.RemoveAt(SoundIndexesDefaulted.Count - 1);
+
+            while (SoundIndexesDefaulted.Count < SoundList.Count)
+                SoundIndexesDefaulted.Add(false);
+
+            for (int i = 0; i < SoundList.Count; i++)
+            {
+                if (SoundIndexesDefaulted[i]) continue;
+                SoundList[i].SetDefaults();
+                SoundIndexesDefaulted[i] = true;
+            }
+
+
+
+            while (VoiceIndexesDefaulted.Count > VoiceList.Count)
+                VoiceIndexesDefaulted.RemoveAt(VoiceIndexesDefaulted.Count - 1);
+
+            while (VoiceIndexesDefaulted.Count < VoiceList.Count)
+                VoiceIndexesDefaulted.Add(false);
+
+            for (int i = 0; i < VoiceList.Count; i++)
+            {
+                if (VoiceIndexesDefaulted[i]) continue;
+                VoiceList[i].SetDefaults();
+                VoiceIndexesDefaulted[i] = true;
+            }
         }
 
 
@@ -76,12 +160,20 @@ namespace PizzaFam
         [RegisterCommand(Name = "Sounds.Play", MinArgCount = 1, MaxArgCount = 1)]
         private static void CommandSoundPlay(CommandArg[] args) => Play(args[0].String);
 
+        [RegisterCommand(Name = "Sounds.PlayVoice", MinArgCount = 1, MaxArgCount = 1)]
+        private static void CommandVoicePlay(CommandArg[] args) => PlayVoice(args[0].String);
+
         [RegisterCommand(Name = "Sounds.List", MaxArgCount = 0)]
         private static void CommandSoundList(CommandArg[] args)
         {
-            Terminal.Log("Initialized sounds:");
+            Terminal.Log("Initialized sounds effects:");
             foreach (var sound in NameToSound)
-                Terminal.Log($"- {sound.Key} ({sound.Value.RandomClips.Length} clips)");
+                Terminal.Log($"- {sound.Key}");
+            Terminal.Log("");
+
+            Terminal.Log("Initialized voice lines:");
+            foreach (var line in NameToVoice)
+                Terminal.Log($"- {line.Key} ({line.Value.RandomClips.Length} clips, {line.Value.ChanceOfPlaying} chance of playing");
             Terminal.Log("");
         }
     }
